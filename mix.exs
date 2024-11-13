682e39f147139b96a94ca4a34939dab0a687f673
Azizul Karim <karimazizul1@gmail.com>
2024-11-13 23:44:42 +0600

    chore : add instructor package

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule Setil.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:instructor, "~> 0.0.5"}
     ]
   end
 
