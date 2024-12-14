75c9b890d165d0be5bb60b77e3e1c12a15546246
Azizul Karim <karimazizul1@gmail.com>
2024-12-14 20:23:44 +0600

    feat : homepage

--- lib/setil_web/controllers/page_html.ex
+++ lib/setil_web/controllers/page_html.ex
@@ -5,6 +5,59 @@ defmodule SetilWeb.PageHTML do
   See the `page_html` directory for all templates available.
   """
   use SetilWeb, :html
+  use Phoenix.Component
 
   embed_templates "page_html/*"
+
+  attr :title, :string, required: true
+  attr :status, :string, required: true
+
+  def feature_card(assigns) do
+    assigns = assign(assigns, :is_available, assigns.status == "Available Now")
+
+    ~H"""
+    <div class={[
+      "p-6 border-4 border-black shadow-brutal select-none cursor-default",
+      (@is_available && "bg-white") || "bg-blue-50",
+      "transform hover:-translate-y-1 transition-transform"
+    ]}>
+      <h3 class="font-mono font-bold text-xl mb-3"><%= @title %></h3>
+      <p class={[
+        "font-mono text-sm",
+        (@is_available && "text-blue-600") || "text-blue-400"
+      ]}>
+        <%= @status %>
+      </p>
+    </div>
+    """
+  end
+
+  def features do
+    [
+      %{
+        title: "Match Heading with Passage",
+        status: "Available Now"
+      },
+      %{
+        title: "Multiple Choice Questions",
+        status: "Coming Soon"
+      },
+      %{
+        title: "True/False/Not Given",
+        status: "Coming Soon"
+      },
+      %{
+        title: "Fill in the Blanks",
+        status: "Coming Soon"
+      },
+      %{
+        title: "Sentence Completion",
+        status: "Coming Soon"
+      },
+      %{
+        title: "Summary Writing",
+        status: "Coming Soon"
+      }
+    ]
+  end
 end
