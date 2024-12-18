c88a0426a0f9d907358aecf133c9efd3a35bea4e
Azizul Karim <karimazizul1@gmail.com>
2024-12-19 00:17:59 +0600

    ui : app home : add account menu to homepage

--- lib/setil_web/live/app_home_live.ex
+++ lib/setil_web/live/app_home_live.ex
@@ -2,8 +2,42 @@ defmodule SetilWeb.AppHomeLive do
   use SetilWeb, :live_view
 
   def mount(_params, session, socket) do
-    preferred_name = session["preferred_name"]
+    {:ok, assign(socket, initial_state(session))}
+  end
 
-    {:ok, assign(socket, :preferred_name, preferred_name)}
+  def handle_event("show_account_dropdown", _unsigned_params, socket) do
+    {:noreply, assign(socket, show_account_dropdown: true)}
+  end
+
+  def handle_event("goto-profile", _unsigned_params, socket) do
+    {:noreply, assign(socket, show_account_dropdown: false)}
+  end
+
+  def handle_event("goto-logout", _unsigned_params, socket) do
+    {:noreply, assign(socket, show_account_dropdown: false)}
+  end
+
+  def handle_event("close-account-dropdown", _unsigned_params, socket) do
+    {:noreply, assign(socket, show_account_dropdown: false)}
+  end
+
+  def initial_state(session) do
+    %{show_account_dropdown: false, preferred_name: session["preferred_name"]}
+  end
+
+  # Components
+
+  attr :label, :string, required: true
+  attr :icon, :string, required: true
+  attr :phx_click, :string, required: true
+
+  def account_dropdown_item(assigns) do
+    ~H"""
+    <div class="flex items-center px-2 py-2 gap-2 text-gray-700 hover:bg-gray-100 border-2 border-gray-900 shadow-lg shadow-sm-brutal hover:translate-x-1 hover:translate-y-1 transition-all cursor-pointer">
+      <.icon name={@icon} class="h-5 w-5" />
+
+      <button phx-click={@phx_click}><%= @label %></button>
+    </div>
+    """
   end
 end
