5df0319678fd50009a2cbe65bb47a4ee6d0a878f
Azizul Karim <karimazizul1@gmail.com>
2024-12-23 21:46:15 +0600

    refactor : post login routes and app under auth

--- lib/setil_web/router.ex
+++ lib/setil_web/router.ex
@@ -24,7 +24,7 @@ defmodule SetilWeb.Router do
   end
 
   scope "/app", SetilWeb do
-    pipe_through [:browser]
+    pipe_through [:browser, :require_authenticated_user]
 
     live "/", AppHomeLive
     live "/match-heading", MatchHeadingLive
--- lib/setil_web/user_auth.ex
+++ lib/setil_web/user_auth.ex
@@ -83,7 +83,7 @@ defmodule SetilWeb.UserAuth do
     conn
     |> renew_session()
     |> delete_resp_cookie(@remember_me_cookie)
-    |> redirect(to: ~p"/")
+    |> redirect(to: ~p"/app/users/log_in")
   end
 
   @doc """
@@ -225,5 +225,5 @@ defmodule SetilWeb.UserAuth do
 
   defp maybe_store_return_to(conn), do: conn
 
-  defp signed_in_path(_conn), do: ~p"/"
+  defp signed_in_path(_conn), do: ~p"/app/"
 end
