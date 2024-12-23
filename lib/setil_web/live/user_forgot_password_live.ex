69e76b2df276fbbae5c0b5d06cc8508b1ab363ba
Azizul Karim <karimazizul1@gmail.com>
2024-12-23 21:35:29 +0600

    refactor : forgot password page

--- lib/setil_web/live/user_forgot_password_live.ex
+++ lib/setil_web/live/user_forgot_password_live.ex
@@ -5,24 +5,31 @@ defmodule SetilWeb.UserForgotPasswordLive do
 
   def render(assigns) do
     ~H"""
-    <div class="mx-auto max-w-sm">
-      <.header class="text-center">
-        Forgot your password?
-        <:subtitle>We'll send a password reset link to your inbox</:subtitle>
-      </.header>
+    <div class="mx-auto max-w-7xl min-h-screen px-4 py-8 sm:px-6 lg:px-8">
+      <div class="mx-auto max-w-sm font-mono">
+        <.link
+          href={~p"/"}
+          class="block w-max mx-auto py-4 md:py-0 md:mx-0 text-4xl md:text-2xl font-mono font-bold text-zinc-900 mb-12"
+          aria-label="SETIL Home"
+        >
+          SETIL
+        </.link>
+        <div>
+          <.header class="text-center">
+            Forgot your password?
+            <:subtitle>We'll send a password reset link to your inbox</:subtitle>
+          </.header>
 
-      <.simple_form for={@form} id="reset_password_form" phx-submit="send_email">
-        <.input field={@form[:email]} type="email" placeholder="Email" required />
-        <:actions>
-          <.button phx-disable-with="Sending..." class="w-full">
-            Send password reset instructions
-          </.button>
-        </:actions>
-      </.simple_form>
-      <p class="text-center text-sm mt-4">
-        <.link href={~p"/app/users/register"}>Register</.link>
-        | <.link href={~p"/app/users/log_in"}>Log in</.link>
-      </p>
+          <.simple_form for={@form} id="reset_password_form" phx-submit="send_email">
+            <.input field={@form[:email]} type="email" placeholder="Email" required />
+            <:actions>
+              <.button phx-disable-with="Sending..." class="w-full">
+                Send password reset instructions
+              </.button>
+            </:actions>
+          </.simple_form>
+        </div>
+      </div>
     </div>
     """
   end
